e1d85f81b724e1ac1dc9953882973a14bd42654e
Khalid <alhajr20@gmail.com>
2021-04-17 15:30:15 +0300

    Added new pages

---
+++ niederkruechten.php
@@ -0,0 +1,62 @@
+<?php
+    $promoTitle = 'Schlüsseldienst in Niederkrüchten';
+    $promoDesc = 'Unser 24h Schlüsseldienst hilft Ihnen jederzeit sofort bei Ihrem Problem!';
+
+    $phoneLink = 'tel:89287888888';
+    $phoneText = '8(928)788-88-88';
+
+    $advantagesFirstTitle = 'Standort';
+    $advantagesFirstDescr = 'Wir sind in Ihrer Nähe';
+    $advantagesSecondTitle = 'Geprüft und Qualifiziert';
+    $advantagesSecondDescr = 'Handwerkerkammer Mitglied';
+    $advantagesThirdTitle = 'Blitzschnell vor Ort';
+    $advantagesThirdDescr = 'In 20 - 40 min. vor Ort';
+
+    $serviceFirstTitle = 'Aufsperrdienst';
+    $serviceFirstText = 'Unser speziell ausgebildetes Team öffnet problemlos zugefallene und versperrte Türen. Wir bieten Ihnen zu 99% schadenfreie Öffnungen an.
+    Sie können innerhalb von 20-30 min. mit einem unserer Mitarbeiter rechnen.';
+    $serviceSecondTitle = 'Tresoröffnung';
+    $serviceSecondText = 'Unsere Mitarbeiter sind auf alle Sicherheitsstufen der Tresoröffnung spezialisiert. Zu 95% werden die Tresoröffnungen beschädigungsfrei durchgeführt. Neben der Öffnung von Tresoren bieten wir auch noch die Reparatur an.';
+    $serviceThirdTitle = 'Türöffnung';
+    $serviceThirdText = 'Zugefallene und versperrte Türen zu öffnen ist für unser Team kein Problem! Unser Team wird Sie mit Ihrer Pünktlichkeit und dem sauberen Arbeitsumgang überzeugen!
+    Rufen Sie uns an und überzeugen Sie sich selbst!';
+
+    $aboutUsText = 'Sie müssen in Ihre Wohnung, Ihr Zuhause oder Ihr Auto, aber Sie wurden ausgesperrt? Wir bieten Ihnen Dienste von professionellen Schlossern, die Sie so schnell wie möglich aus Ihren Schwierigkeiten herausholen können. Wohn-, Geschäftsgebäude und Autos werden von zertifizierten Technikern abgedeckt.<br><br>
+    Haftungsausschluss.';
+
+    $reviewsFirstName = 'Angela Bauer';
+    $reviewsFirstText = 'Ich war auf dem Weg zur Arbeit als mir die Türe zugefallen ist. Der Schlüsseldienst kam zum Glück innerhalb von 20 Minuten an, da ich mich nicht verspüren wollte hat der Mitarbeiter so schnell wie möglich die Türe geöffnet. Super Preis!  Danke für die schnelle Hilfe!';
+    $reviewsSecondName = 'Thomas Specht';
+    $reviewsSecondText = 'Dieser Schlüsseldienst ist sehr empfehlenswert! Sehr kinderfreundlich, rasch und noch dazu eine Türöffnung ohne Beschädigung. Vielen Dank!';
+    $reviewsThirdName = 'Andreas Martin';
+    $reviewsThirdText = 'Leider ist durch einen Windstoß meine Türe zugefallen, ich war panisch und brauchte sofort Hilfe. Der Schlüsseldienst kam sehr schnell und leistete Top Arbeit. Empfehlenswert!';
+
+    $questionFirst = 'Wie finde ich den besten Schlosser?';
+    $answerFirst = 'Sie sollten zuerst schauen welche Art von Dienstleistung Sie benötigen und ob der von Ihnen ausgewählte Schlüsseldienst auch diese gewünschte Leistung erbringen kann.
+    Sie profitieren immer davon wenn Sie wissen was Sie von Ihrem technischen Schloss benötigen, ob Sie von zu Hause ausgesperrt sind oder versehentlich ein Schloss aufgebrochen haben.';
+    $questionSecond = 'Was kann ein Schlosser tun?';
+    $answerSecond = 'Ein professioneller Schlosser ist jemand, der für die Arbeit mit dem vollständigen Werkzeug und Material ausgestattet ist um Schlösser von Alltagsgegenständen wie Türen, Fenstern und Safes zu bedienen.';
+    $questionThird = 'Was ist der durchschnittliche Preis für einen Schlosstausch?';
+    $answerThird = 'Der durchschnittliche Preis kann je nach Bedarf Ihres Schlosses ab 49€ Betriebskosten + MwSt. sehr unterschiedlich sein. Falls Sie ausgesperrt wurden oder zusätzliche Schlösser an Ihren Türen zu Hause benötigen um für zusätzliche Sicherheit zu sorgen könnte je nach Ihrer Situation der Preis erheblich variieren.';
+    $questionFourth = 'Was sind verschieden Arten von Schlüsseldienst Technikern?';
+    $answerFourth = 'Professionelle Schlüsseldienst Teams bieten eine Reihe von verschiedenen Techniker-Schlosser und Schlosser-Dienstleistungen. 
+    Unser Team bietet Ihnen einen 24h Service mit einem top ausgebildetem Team!';
+    $questionFiveth = 'Was erwarten Sie vom Technikerservice?';
+    $answerFiveth = 'Sie können von unserem Team pünktliche, zuverlässige und professionelle Dienstleistungen erwarten. Unser Team sorgt für Ihre Zufriedenheit!';
+    $questionSixth = 'Was ist unser Schlosser-Unternehmensprogramm?';
+    $answerSixth = 'Wir sind 24 Stunden am Tag, 7 Tage die Woche für Sie da. Rufen Sie unser Team jederzeit an!';
+?>
+
+<?php
+require_once('./header.php');
+require_once('./promo.php');
+require_once('./advantages.php');
+require_once('./service.php');
+require('./ourContacts.php');
+require_once('./aboutUs.php');
+require_once('./reviews.php');
+require('./ourContacts.php');
+require_once('./question.php');
+require_once('./payment.php');
+require_once('./footer.php');
+?>
